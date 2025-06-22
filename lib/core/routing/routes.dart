cfb9e5e62a4f1015fdcbad855a8a138cad85a223
AbubakerSallam <abubaker4sallam@gmail.com>
2025-06-22 23:06:12 +0300

    Revert "added splash , core files and assets "

--- lib/core/routing/routes.dart
+++
@@ -1,8 +0,0 @@
-class Routes {
-  static const String defaultScreen = '/';
-  static const String splashScreen = '/splash';
-  static const String onBoardingScreen = '/onBoardingScreen';
-  static const String loginScreen = '/loginScreen';
-  static const String signUpScreen = '/signUpScreen';
-  static const String homeScreen = '/homeScreen';
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,14 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shop_app/core/routing/app_router.dart';
 
-import 'package:shop_app/shop_app.dart';
+import 'package:shop_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( ShopApp(appRouter: AppRouter(),));
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
